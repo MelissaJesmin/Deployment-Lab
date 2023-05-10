1ee3bf8dada18879fe16ea22d7de1e6b7b1b2548
MelissaJesmin <melissa.jesmin25@gmail.com>
2023-05-10 12:05:27 -0400

    Initial Commit

--- public/main.js
+++ public/main.js
@@ -1,1 +1,5 @@
-app.use(express.static(`${__dirname}/public`))
+const btn = document.querySelector('button')
+
+const clickHandler = () => alert('The cat name is: Rascal')
+
+btn.addEventListener('click', clickHandler)
